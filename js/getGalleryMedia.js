b9a7890bd3c73b5b8f9af45e630beed4ab2af7b8
Merilax <merilaxreloaded@gmail.com>
2023-03-26 18:41:23 +0200

    modified:   css/gallery.css 	modified:   css/master.css 	modified:   index.html 	modified:   js/getGalleryMedia.js 	modified:   js/jsonGallery.js 	modified:   sub/gallery.html

--- js/getGalleryMedia.js
+++ js/getGalleryMedia.js
@@ -1,26 +1,34 @@
 json.forEach(artist => {
+    // Artist page
 
-    var main = $(`<main></main>"`).attr({
+    //var main = $(`<main></main>"`).css( "display", "none");
+    /*.attr({
         id: artist.id,
         class: "mainArtist",
-    }).css({ "display": "none" });
+    }).css({ "display": "none" });*/
+
+    var main = $('#' + artist.id);
+    main.css({ display: "none" });
 
     var intro = $(`<div></div>`).addClass("artIntro");
 
+    // Artist header
     var artistimg = $(`<img />`).attr({
         src: artist.intro.img.src,
         alt: artist.intro.img.alt
     });
     var artisth1 = $(`<h1></h1>`).text(artist.intro.h1);
     var artisth4 = $(`<h4></h4>`).text(artist.intro.h4);
-    var artistp = $(`<p></p>`).text(artist.intro.p).attr({ style: "white-space: pre-wrap" });
+    var artistp = $(`<p></p>`).text(artist.intro.p);
 
     var gallery = $(`<div></div>`).attr({ class: "artGallery" });
 
-    $("body").append(main);
-    $(main).append(intro, gallery);
+    //$("body").append(main);
+    main.prepend(intro);
     $(intro).append(artistimg, artisth1, artisth4, artistp);
+    main.append(gallery);
 
+    // Entries
     artist.gallery.forEach(mech => {
         var entry = $(`<div></div>`).attr({ class: "entry" });
         var media = $(`<div></div>`).attr({ class: "media" });
@@ -29,10 +37,12 @@ json.forEach(artist => {
         $(gallery).append(entry);
         $(entry).append(entryh3, media);
 
+
         try {
             var mainMedia = $(`<span></span>`).attr({ class: "mainMedia" });
             var extraMedia = $(`<span></span>`).attr({ class: "extraMedia" });
 
+            // Main window
             var mainimg = $(`<img />`).attr({
                 src: mech.media[0],
                 alt: "1",
@@ -45,21 +55,35 @@ json.forEach(artist => {
             $(media).append(mainMedia, extraMedia);
             $(mainMedia).append(mainimg);
 
+            // Extra images
             for (let i = 1; i < mech.media.length + 1; i++) {
                 const mechimg = mech.media[i - 1];
 
                 var extralink = $(`<a></a>`).attr({
                     href: mechimg,
                     target: "_blank"
-                })
+                });
 
                 var linkimg = $(`<img />`).attr({
-                    src: mechimg,
                     alt: i,
                     id: mech.id + "_" + i,
                     onmouseenter: `switchImage( "${mech.id}", "${mech.id + '_' + i}" )`,
                     loading: "lazy"
-                })
+                });
+
+                try {
+                    // Thumbnail
+                    const mechthumb = mech.thumb[i - 1];
+                    linkimg.attr({
+                        src: mechthumb
+                    });
+                } catch (error) {
+                    // If no thumbnail use main img
+                    linkimg.attr({
+                        src: mechimg
+                    });
+                }
+
 
                 $(extraMedia).append(extralink);
                 $(extralink).append(linkimg);
--- js/jsonGallery.js
+++ js/jsonGallery.js
@@ -16,6 +16,9 @@ const json = [
                 "h3": "Kronos",
                 "media": [
                     "https://i.ibb.co/pnv8FQk/kronos1.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/K5gnfvp/kronos1.png"
                 ]
             },
             {
@@ -26,6 +29,12 @@ const json = [
                     "https://i.ibb.co/8xSjGHf/child-Of-The-Monarch2.png",
                     "https://i.ibb.co/7rvqpwW/child-Of-The-Monarch3.png",
                     "https://i.ibb.co/FDTx1QJ/child-Of-The-Monarch4.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/Vmf0GtN/child-Of-The-Monarch1.png",
+                    "https://i.ibb.co/56FhJkK/child-Of-The-Monarch2.png",
+                    "https://i.ibb.co/FYFjmN0/child-Of-The-Monarch3.png",
+                    "https://i.ibb.co/2kcdspr/child-Of-The-Monarch4.png"
                 ]
             },
             {
@@ -36,6 +45,12 @@ const json = [
                     "https://i.ibb.co/GTgCvPp/death-Blossom2.png",
                     "https://i.ibb.co/NFGmJSn/death-Blossom3.png",
                     "https://i.ibb.co/DC2gbgH/death-Blossom4.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/3MbGWBM/death-Blossom1.png",
+                    "https://i.ibb.co/f4fNFHV/death-Blossom2.png",
+                    "https://i.ibb.co/CMN9K50/death-Blossom3.png",
+                    "https://i.ibb.co/x7JDyNd/death-Blossom4.png"
                 ]
             },
             {
@@ -47,6 +62,13 @@ const json = [
                     "https://i.ibb.co/6rfXxLt/schneejagdtiger3.png",
                     "https://i.ibb.co/dcRMq5V/schneejagdtiger4.png",
                     "https://i.ibb.co/vHj2mhP/schneejagdtiger5.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/s9MtFBS/schneejagdtiger1.png",
+                    "https://i.ibb.co/TmZ5GML/schneejagdtiger2.png",
+                    "https://i.ibb.co/xFhDJys/schneejagdtiger3.png",
+                    "https://i.ibb.co/ZWZLGCP/schneejagdtiger4.png",
+                    "https://i.ibb.co/mBkKBk2/schneejagdtiger5.png"
                 ]
             },
             {
@@ -57,6 +79,12 @@ const json = [
                     "https://i.ibb.co/gvYfS3L/starstruck2.png",
                     "https://i.ibb.co/vdQrGr5/starstruck3.png",
                     "https://i.ibb.co/bKY0S1R/starstruck4.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/VwYJW87/starstruck1.png",
+                    "https://i.ibb.co/fxQ6d0x/starstruck2.png",
+                    "https://i.ibb.co/QJq1GL9/starstruck3.png",
+                    "https://i.ibb.co/3WmRGj8/starstruck4.png"
                 ]
             },
             {
@@ -68,6 +96,13 @@ const json = [
                     "https://i.ibb.co/ZNJWwCq/gilgamesh-MK2-3.png",
                     "https://i.ibb.co/dB5mP8m/gilgamesh-MK2-4.png",
                     "https://i.ibb.co/PQSM7k3/gilgamesh-MK2-5.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/SwTrM35/gilgamesh-MK2-1.png",
+                    "https://i.ibb.co/cQ23kyY/gilgamesh-MK2-2.png",
+                    "https://i.ibb.co/JmbxjzK/gilgamesh-MK2-3.png",
+                    "https://i.ibb.co/cNnbT1T/gilgamesh-MK2-4.png",
+                    "https://i.ibb.co/pytvrxb/gilgamesh-MK2-5.png"
                 ]
             },
             {
@@ -81,6 +116,15 @@ const json = [
                     "https://i.ibb.co/MhWHM9J/dragonite5.png",
                     "https://i.ibb.co/LRKp7MQ/dragonite6.png",
                     "https://i.ibb.co/1bvQ8bB/dragonite7.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/gDSt58r/dragonite1.png",
+                    "https://i.ibb.co/QXCr1VR/dragonite2.png",
+                    "https://i.ibb.co/xfFBpP8/dragonite3.png",
+                    "https://i.ibb.co/km3SQ63/dragonite4.png",
+                    "https://i.ibb.co/FJsx2Yb/dragonite5.png",
+                    "https://i.ibb.co/R3Tz4kP/dragonite6.png",
+                    "https://i.ibb.co/HzZN1F1/dragonite7.png"
                 ]
             },
             {
@@ -91,6 +135,12 @@ const json = [
                     "https://i.ibb.co/QMVKGsS/gallahad2.png",
                     "https://i.ibb.co/zrGY6tf/gallahad3.png",
                     "https://i.ibb.co/CMX0h6y/gallahad4.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/GTWf5HS/gallahad1.png",
+                    "https://i.ibb.co/xscBRMS/gallahad2.png",
+                    "https://i.ibb.co/BcbnZ6w/gallahad3.png",
+                    "https://i.ibb.co/GpbxNF4/gallahad4.png"
                 ]
             },
             {
@@ -106,6 +156,17 @@ const json = [
                     "https://i.ibb.co/dgyR4vY/volgograd7.png",
                     "https://i.ibb.co/CnNXpx6/volgograd8.png",
                     "https://i.ibb.co/rx5VRQ2/volgograd9.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/q0myt3R/volgograd1.png",
+                    "https://i.ibb.co/wJTFjTT/volgograd2.png",
+                    "https://i.ibb.co/dJBMnyy/volgograd3.png",
+                    "https://i.ibb.co/89ftL2R/volgograd4.png",
+                    "https://i.ibb.co/BGGMGgk/volgograd5.png",
+                    "https://i.ibb.co/NyLWD8m/volgograd6.png",
+                    "https://i.ibb.co/k9jsB7x/volgograd7.png",
+                    "https://i.ibb.co/87Pkg5F/volgograd8.png",
+                    "https://i.ibb.co/Lnk5XDk/volgograd9.png"
                 ]
             },
             {
@@ -117,6 +178,13 @@ const json = [
                     "https://i.ibb.co/VwpQr88/kamchatka2.png",
                     "https://i.ibb.co/tJYNCQk/kamchatka3.png",
                     "https://i.ibb.co/vZw4JFC/kamchatka4.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/GsPMqnZ/kamchatka1.png",
+                    "https://i.ibb.co/VYc9tzy/kamchatka2.png",
+                    "https://i.ibb.co/GJGyhN6/kamchatka3.png",
+                    "https://i.ibb.co/wLtR840/kamchatka4.png",
+                    "https://i.ibb.co/KwQmr0W/kamchatka5.png"
                 ]
             },
             {
@@ -129,6 +197,14 @@ const json = [
                     "https://i.ibb.co/FBXQ0c6/carnivore4.png",
                     "https://i.ibb.co/7vGTBtf/carnivore5.png",
                     "https://i.ibb.co/6nXyc2F/carnivore6.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/122DcCX/carnivore1.png",
+                    "https://i.ibb.co/wLwDnqj/carnivore2.png",
+                    "https://i.ibb.co/YLvNKHD/carnivore3.png",
+                    "https://i.ibb.co/2WXkCns/carnivore4.png",
+                    "https://i.ibb.co/kK3Xky1/carnivore5.png",
+                    "https://i.ibb.co/FYrSwVx/carnivore6.png"
                 ]
             },
             {
@@ -143,6 +219,16 @@ const json = [
                     "https://i.ibb.co/nQX29qd/eye-Of-The-Monarch6.png",
                     "https://i.ibb.co/n8t8Vc8/eye-Of-The-Monarch7.png",
                     "https://i.ibb.co/ScdYJmM/eye-Of-The-Monarch8.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/qxFLXsw/eye-Of-The-Monarch1.png",
+                    "https://i.ibb.co/G29LGZ6/eye-Of-The-Monarch2.png",
+                    "https://i.ibb.co/BGY3x1D/eye-Of-The-Monarch3.png",
+                    "https://i.ibb.co/s2RzpDL/eye-Of-The-Monarch4.png",
+                    "https://i.ibb.co/680Bshk/eye-Of-The-Monarch5.png",
+                    "https://i.ibb.co/3zyVB4d/eye-Of-The-Monarch6.png",
+                    "https://i.ibb.co/2Y4XQPb/eye-Of-The-Monarch7.png",
+                    "https://i.ibb.co/Lxwr71t/eye-Of-The-Monarch8.png"
                 ]
             },
             {
@@ -151,6 +237,10 @@ const json = [
                 "media": [
                     "https://i.ibb.co/ygTDVG3/genocide-Type-A1.png",
                     "https://i.ibb.co/tbQcsV2/genocide-Type-A2.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/8gH3NF4/genocide-Type-A1.png",
+                    "https://i.ibb.co/Jq93JnD/genocide-Type-A2.png"
                 ]
             },
             {
@@ -160,6 +250,11 @@ const json = [
                     "https://i.ibb.co/N12pS0f/selece1.png",
                     "https://i.ibb.co/MRZqPby/selece2.png",
                     "https://i.ibb.co/G0SSHpL/selece3.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/SdZjS0G/selece1.png",
+                    "https://i.ibb.co/KD0bxYV/selece2.png",
+                    "https://i.ibb.co/w0WWqJt/selece3.png"
                 ]
             },
             {
@@ -171,6 +266,13 @@ const json = [
                     "https://i.ibb.co/nmGsLfb/novosibirsk3.png",
                     "https://i.ibb.co/hWjsQKV/novosibirsk4.png",
                     "https://i.ibb.co/bR2Qgy2/novosibirsk5.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/1fv3Xyc/novosibirsk1.png",
+                    "https://i.ibb.co/CQn578Q/novosibirsk2.png",
+                    "https://i.ibb.co/WyrhvVK/novosibirsk3.png",
+                    "https://i.ibb.co/rpXqxdR/novosibirsk4.png",
+                    "https://i.ibb.co/XXYJ1pH/novosibirsk5.png"
                 ]
             },
             {
@@ -180,6 +282,11 @@ const json = [
                     "https://i.ibb.co/kXgLBfQ/sevastopol1.png",
                     "https://i.ibb.co/YphZZ34/sevastopol2.png",
                     "https://i.ibb.co/H2pxg7q/sevastopol3.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/4JM3ws5/sevastopol1.png",
+                    "https://i.ibb.co/rxv1JBc/sevastopol2.png",
+                    "https://i.ibb.co/dGLv9nS/sevastopol3.png"
                 ]
             },
             {
@@ -189,6 +296,11 @@ const json = [
                     "https://i.ibb.co/cvwFp8H/horizon-Gun-Carriage1.png",
                     "https://i.ibb.co/VCbf4Zj/horizon-Gun-Carriage2.png",
                     "https://i.ibb.co/CztwYrG/horizon-Gun-Carriage3.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/GTwwxR3/horizon-Gun-Carriage1.png",
+                    "https://i.ibb.co/CPRWPqc/horizon-Gun-Carriage2.png",
+                    "https://i.ibb.co/31r8vYw/horizon-Gun-Carriage3.png"
                 ]
             },
             {
@@ -201,6 +313,14 @@ const json = [
                     "https://i.ibb.co/8MYYKNF/sorrowful-Knight4.png",
                     "https://i.ibb.co/wBHhyDC/sorrowful-Knight5.png",
                     "https://i.ibb.co/JHbMBmh/sorrowful-Knight6.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/GTmq6bM/sorrowful-Knight1.png",
+                    "https://i.ibb.co/QftyzH8/sorrowful-Knight2.png",
+                    "https://i.ibb.co/PYmxtNK/sorrowful-Knight3.png",
+                    "https://i.ibb.co/1zF971p/sorrowful-Knight4.png",
+                    "https://i.ibb.co/86gJsRk/sorrowful-Knight5.png",
+                    "https://i.ibb.co/pJPZPPb/sorrowful-Knight6.png"
                 ]
             },
             {
@@ -210,6 +330,11 @@ const json = [
                     "https://i.ibb.co/1rHx0m6/nevertale1.png",
                     "https://i.ibb.co/z579xcY/nevertale2.png",
                     "https://i.ibb.co/KqTnvQm/nevertale3.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/7SfDQrn/nevertale1.png",
+                    "https://i.ibb.co/cx5DRLf/nevertale2.png",
+                    "https://i.ibb.co/KwrzFf6/nevertale3.png"
                 ]
             },
             {
@@ -220,6 +345,12 @@ const json = [
                     "https://i.ibb.co/3fZVVJP/evertale2.png",
                     "https://i.ibb.co/qRp1qMv/evertale3.png",
                     "https://i.ibb.co/ZXZrszK/evertale4.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/XxnBgBY/evertale1.png",
+                    "https://i.ibb.co/7zw70Sk/evertale2.png",
+                    "https://i.ibb.co/dJYfPzV/evertale3.png",
+                    "https://i.ibb.co/r6GfSYB/evertale4.png"
                 ]
             },
             {
@@ -230,6 +361,12 @@ const json = [
                     "https://i.ibb.co/FW14w6v/K-MObj279-2.png",
                     "https://i.ibb.co/C1C3Q3D/K-MObj279-3.png",
                     "https://i.ibb.co/0ZvpgN5/K-MObj279-4.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/t46frTS/K-MObj279-1.png",
+                    "https://i.ibb.co/RCcyHXS/K-MObj279-2.png",
+                    "https://i.ibb.co/9WScJCY/K-MObj279-3.png",
+                    "https://i.ibb.co/7KQdybq/K-MObj279-4.png"
                 ]
             },
             {
@@ -238,6 +375,10 @@ const json = [
                 "media": [
                     "https://i.ibb.co/9tdd20K/holiday1.png",
                     "https://i.ibb.co/Bt0zQ4q/holiday2.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/d6LLGw6/holiday1.png",
+                    "https://i.ibb.co/Srg0Fp9/holiday2.png"
                 ]
             },
             {
@@ -249,6 +390,13 @@ const json = [
                     "https://i.ibb.co/bNqm26m/K-IHeavy-Works-Unit3.png",
                     "https://i.ibb.co/BPXy0s4/K-IHeavy-Works-Unit4.png",
                     "https://i.ibb.co/Stwh8BZ/K-IHeavy-Works-Unit5.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/bPLmL83/K-IHeavy-Works-Unit1.png",
+                    "https://i.ibb.co/Hh01JB2/K-IHeavy-Works-Unit2.png",
+                    "https://i.ibb.co/w6Fq0FL/K-IHeavy-Works-Unit3.png",
+                    "https://i.ibb.co/s6B8Z20/K-IHeavy-Works-Unit4.png",
+                    "https://i.ibb.co/zSmXSMy/K-IHeavy-Works-Unit5.png"
                 ]
             },
             {
@@ -259,6 +407,12 @@ const json = [
                     "https://i.ibb.co/k8YQ7Zv/ballista-Containment2.png",
                     "https://i.ibb.co/vwYHxsL/ballista-Containment3.png",
                     "https://i.ibb.co/nspvnqm/ballista-Containment4.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/wL7qLCb/ballista-Containment1.png",
+                    "https://i.ibb.co/wMLXc11/ballista-Containment2.png",
+                    "https://i.ibb.co/n84ZCGw/ballista-Containment3.png",
+                    "https://i.ibb.co/Z8cs7rn/ballista-Containment4.png"
                 ]
             },
             {
@@ -266,6 +420,9 @@ const json = [
                 "h3": "Arbiter",
                 "media": [
                     "https://i.ibb.co/zhMjZTY/arbiter1.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/WDJHkV2/arbiter1.png"
                 ]
             },
             {
@@ -284,6 +441,20 @@ const json = [
                     "https://i.ibb.co/17m6XY9/white-Night10.png",
                     "https://i.ibb.co/DYNMH9Y/white-Night11.png",
                     "https://i.ibb.co/Nt2krQJ/white-Night12.png"
+                ],
+                "thumb": [
+                    "https://i.ibb.co/Nn05NM1/white-Night1.png",
+                    "https://i.ibb.co/6Ns89xL/white-Night2.png",
+                    "https://i.ibb.co/fd2cdzK/white-Night3.png",
+                    "https://i.ibb.co/6shJbC7/white-Night4.png",
+                    "https://i.ibb.co/jwRSjdZ/white-Night5.png",
+                    "https://i.ibb.co/DMxTtW8/white-Night6.png",
+                    "https://i.ibb.co/hR1pHnN/white-Night7.png",
+                    "https://i.ibb.co/N1sgSZ4/white-Night8.png",
+                    "https://i.ibb.co/SdkW3K6/white-Night9.png",
+                    "https://i.ibb.co/1GH21Zk/white-Night10.png",
+                    "https://i.ibb.co/dMvLjv0/white-Night11.png",
+                    "https://i.ibb.co/J3Fjks4/white-Night12.png"
                 ]
             }
         ]
